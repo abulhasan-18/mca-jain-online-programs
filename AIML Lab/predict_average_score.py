2593db8f94fff2ccc8b60be9839a85859abdf04d
Mohammed Abulhasan <mdyahasan18@gmail.com>
2025-12-28 09:51:17 +0530

    program 1 to 4 over

---
+++ AIML Lab/predict_average_score.py
@@ -0,0 +1,4 @@
+mark1, mark2, mark3 = map(int, input().split())
+
+average = (mark1 + mark2 + mark3) / 3
+print(average)
---
+++ AIML Lab/user_interest.py
@@ -0,0 +1,7 @@
+interest = input()
+if(interest == "data"):
+    print("Data Science")
+elif(interest == "ai"):
+    print("Artificial Intelligence")
+else:
+    print("Programming Basics")
